97bfad60d2f1df3eb4a07147ed55b6c316597953
fbnmlnss <fabimilanes@gmail.com>
2024-02-26 21:22:52 -0600

    changed .properties to .yml

---
+++ src/main/java/com/media/groove/GrooveApplication.java
@@ -0,0 +1,13 @@
+package com.media.groove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrooveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrooveApplication.class, args);
+	}
+
+}
