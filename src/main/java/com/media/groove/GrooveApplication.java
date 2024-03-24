0685cc6422d7e90b9912f4fe9bac229704e55f44
fbnmlnss <fabimilanes@gmail.com>
2024-03-24 16:57:26 -0600

    feat: java fx application integrated with spring boot

--- src/main/java/com/media/groove/GrooveApplication.java
+++ src/main/java/com/media/groove/GrooveApplication.java
@@ -1,5 +1,6 @@
 package com.media.groove;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GrooveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GrooveApplication.class, args);
+		Application.launch(MediaPlayerApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/media/groove/WelcomePageController.java
@@ -0,0 +1,27 @@
+package com.media.groove;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class WelcomePageController {
+    @FXML
+    public Button btnLogin;
+
+    @Value("classpath:/hola.fxml")
+    private Resource screenSource;
+    private final StageInitializer stageInitializer;
+
+    public WelcomePageController(StageInitializer stageInitializer) {
+        this.stageInitializer = stageInitializer;
+    }
+
+    public void go() throws IOException {
+        stageInitializer.switchScene(screenSource);
+    }
+}
