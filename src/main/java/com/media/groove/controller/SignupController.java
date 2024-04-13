34f9c5eeffb38dae1aa04ec1ddd731269a9aa13b
fbnmlnss <fabimilanes@gmail.com>
2024-04-12 20:28:50 -0600

    feat: user can register and validations for registration added

--- src/main/java/com/media/groove/controller/SignupController.java
+++ src/main/java/com/media/groove/controller/SignupController.java
@@ -2,8 +2,10 @@ package com.media.groove.controller;
 
 import com.media.groove.entity.User;
 import com.media.groove.service.UserService;
+import javafx.beans.Observable;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import org.springframework.stereotype.Controller;
@@ -14,6 +16,9 @@ public class SignupController {
 
     @FXML
     public PasswordField password;
+
+    @FXML
+    public PasswordField passwordConfirmation;
 
     @FXML
     public TextField name;
@@ -27,6 +32,21 @@ public class SignupController {
     @FXML
     public Button signupButton;
 
+    @FXML
+    public Label lblName;
+
+    @FXML
+    public Label lblLastName;
+
+    @FXML
+    public Label lblUsername;
+
+    @FXML
+    public Label lblPassword;
+
+    @FXML
+    public Label lblPasswordConfirmation;
+
     public SignupController(UserService userService) {
         this.userService = userService;
     }
@@ -35,10 +55,61 @@ public class SignupController {
     public void signUp() {
         User newUser = new User();
 
-        newUser.setName(this.name.getText());
-        newUser.setLastName(this.lastName.getText());
-        newUser.setUsername(this.username.getText());
+        if (this.signUpInformationIsValid()) {
+            newUser.setName(this.name.getText());
+            newUser.setLastName(this.lastName.getText());
+            newUser.setUsername(this.username.getText());
 
-        this.userService.createUser(newUser, this.password.getText());
+            this.userService.createUser(newUser, this.password.getText());
+        }
+    }
+
+    private boolean signUpInformationIsValid() {
+        boolean isValid = true;
+
+        if (this.username.getText().isEmpty()) {
+            this.username.getStyleClass().remove("input");
+
+            this.username.getStyleClass().add("error");
+            this.lblUsername.getStyleClass().add("label-error");
+
+            isValid = false;
+        }
+
+        if (this.lastName.getText().isEmpty()) {
+            this.lastName.getStyleClass().remove("input");
+
+            this.lastName.getStyleClass().add("error");
+            this.lblLastName.getStyleClass().add("label-error");
+
+            isValid = false;
+        }
+
+        if (this.name.getText().isEmpty()) {
+            this.name.getStyleClass().remove("input");
+
+            this.name.getStyleClass().add("error");
+            this.lblName.getStyleClass().add("label-error");
+
+            isValid = false;
+        }
+
+        if (this.password.getText().isEmpty() || !this.passwordsMatch()) {
+            this.password.getStyleClass().remove("input");
+            this.passwordConfirmation.getStyleClass().remove("error");
+
+            this.password.getStyleClass().add("error");
+            this.passwordConfirmation.getStyleClass().add("error");
+            this.lblPassword.getStyleClass().add("label-error");
+            this.lblPasswordConfirmation.getStyleClass().add("label-error");
+
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
+    private boolean passwordsMatch() {
+        return (this.password.getText().equals(this.passwordConfirmation.getText()));
     }
 }
